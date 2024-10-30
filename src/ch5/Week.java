ebd34511f3832b8421f372753ff8df7a04f2e427
Admin <Admin@DESKTOP-52351T8>
2024-10-30 18:04:22 +0900

    2024-10-30일 수업내용

---
+++ src/ch5/Week.java
@@ -0,0 +1,15 @@
+package ch5;
+
+public enum Week {	// 열거형 enum, 열거형 이름 Week
+	// 열거 상수 목록 : 제한된 값을 목록을 지정한 것 입니다.
+	MONDAY,
+	TUESDAY,
+	WEDNESDAY,
+	THURSDAY,
+	FRIDAY,
+	SATURDAY,
+	SUNDAY
+	
+	
+
+}
--- src/quiz/LoopQuiz1.java
+++ src/quiz/LoopQuiz1.java
@@ -1,5 +1,7 @@
 package quiz;
 
+import java.util.Arrays;
+
 public class LoopQuiz1 {
 
 	public static void main(String[] args) {
@@ -11,8 +13,53 @@ public class LoopQuiz1 {
 		 * 	3. 번호를 출력하기
 		 * 
 		 */
+		
+		/* 배열을 사용한 코드....
+		 * 배열의 선언...
+		 */
+		int[] lotto = new int[6]; // 정수 타입 배열로 길이가 6인....
+//		int lotto[] = new int[6]; -> 이렇게도 배열선언할 수 있지만 위에께 자주쓰임
+		
+		// 검증 - 중복없은 로또 번호 6개 뽑기....
+	
+		for (int i = 0; i < 6; i++) {
+			lotto[i]= (int)(Math.random() * 45) + 1; // 1 ~ 45
+			
+			for (int j = 0;  j < i; j++) {	// 검색 알고리즘...
+				 if(lotto[j] == lotto[i]) {	
+					 i++;
+					 break;
+				 }		
+		}
+	}
+	System.out.println(Arrays.toString(lotto));
+	
+	//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+	
+	 int[] lotto2 = new int[6];
+     
+     int count = 0;
+     boolean testCheck = true;
+     while (count < 6) {
+        int lottoNum = (int)(Math.random() * 45) + 1;     // 생성
+        for (int i = 0; i < count; i ++) {                // 검증
+           if (lotto2[i] == lottoNum) {
+              testCheck = false;
+              break;
+           }
+        }
+        if (testCheck) {                       			  // 대입
+           lotto2[count] = lottoNum;
+           count++;
+        }
+        testCheck = true;                     			  // 검증 초기화
+     }
+     
+     System.out.println("while을 사용한 경우 : "+Arrays.toString(lotto2));
 
-		System.out.println("ㅡㅡㅡ로또번호게임ㅡㅡㅡㅡ");
+		
+		
+/*		System.out.println("ㅡㅡㅡ로또번호게임ㅡㅡㅡㅡ");
 
 		int lotto1 =0, lotto2 =0, lotto3 =0, lotto4 =0, lotto5 =0, lotto6 =0;
 		int count = 0; // 출력 번호 갯수
@@ -45,7 +92,7 @@ public class LoopQuiz1 {
 		if (count == 6) break;
 
  	}
-
+*/
   }
 
 }
